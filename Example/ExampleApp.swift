fc95342332851527dee1cf671c8a0862dda02a6e
Dan Torres <danstorre@gmail.com>
2024-04-08 19:58:51 -0300

    Initial Commit

---
+++ Example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by Daniel Torres on 4/8/24.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
