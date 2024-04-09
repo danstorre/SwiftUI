c69a1c8d3219c453888b13576d13d8655d5d2e71
Dan Torres <danstorre@gmail.com>
2024-04-09 14:14:50 -0300

    uses state object.

--- Example/ExampleApp.swift
+++ Example/ExampleApp.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 @main
 struct ExampleApp: App {
+    @StateObject var vm = TemperatureViewModel()
+    
     var body: some Scene {
         WindowGroup {
-            ContentView(vm: TemperatureViewModel())
+            ContentView(vm: vm)
         }
     }
 }
